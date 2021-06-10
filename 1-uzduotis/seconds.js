1da242686b9161fb9ed4fbfc146930c160441743
GedasPupa <gedosas@gmail.com>
2021-06-10 16:02:44 +0300

    Bug fix on seconds.js output (format)

--- 1-uzduotis/seconds.js
+++ 1-uzduotis/seconds.js
@@ -9,26 +9,35 @@ let element = document.getElementById('input');
 let outputEl = document.getElementById('output');
 
 function getTime() {
-    if (totalSecs <= 60) { // - seconds go here
+    if (totalSecs < 60) { // - seconds go here
         seconds = totalSecs;
         if (totalSecs < 10) {
             seconds = "0" + totalSecs;
         }
         console.log("'00:00:" + seconds +"'");
-    } else if (totalSecs <= 3600) { // ------------------------------- minutes go here
+    } else if (totalSecs < 3600) { // ------------------------------- minutes go here
         minutes = Math.floor(totalSecs / 60);
         if (minutes < 10) {
             minutes = "0" + minutes;
         }
         seconds = totalSecs % 60;
+        if (seconds < 10 ) {
+            seconds = "0" + seconds;
+        }
         console.log("'00:" + minutes + ":" + seconds +"'");
-    } else if (totalSecs <= 86400) { // ------------------------------ hours go here
+    } else if (totalSecs < 86400) { // ------------------------------ hours go here
         hours = Math.floor(totalSecs / 3600);
         if (hours < 10) {
             hours = "0" + hours;
         }
         minutes = Math.floor((totalSecs % 3600) / 60);
+        if (minutes < 10) {
+            minutes = "0" + minutes;
+        }
         seconds = (totalSecs % 3600) % 60;
+        if (seconds < 10) {
+            seconds = "0" + seconds;
+        }
         console.log("'" + hours + ":" + minutes + ":" + seconds +"'");
     } else { // ------------------------------------------------------- ERROR - too big input
         console.log("ERROR: Please enter less then 86 400 seconds - format is only '24 h 00 minutes 00 seconds!'");
